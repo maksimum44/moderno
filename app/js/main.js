29043a87ef505df7d98a1fd733c604b0a593e96d
maksimum44 <educations.maxim@gmail.com>
2019-12-17 01:15:00 +0200

    added adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -22,18 +22,24 @@ $(".js-range-slider").ionRangeSlider({
 });
 
 $('.icon-th-list').on('click', function(){
-$('.product__item').addClass('list');
-$('.icon-th-list').addClass('active');
-$('.icon-th-large').removeClass('active')
+  $('.product__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large').removeClass('active')
 });
 
 $('.icon-th-large').on('click', function(){
-$('.product__item').removeClass('list');
-$('.icon-th-large').addClass('active');
-$('.icon-th-list').removeClass('active')
-
+  $('.product__item').removeClass('list');
+  $('.icon-th-large').addClass('active');
+  $('.icon-th-list').removeClass('active')
 });
 
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle();
+});
+
+$('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
 
     var mixer = mixitup('.products__inner-box');
 
